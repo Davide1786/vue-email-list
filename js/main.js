080c4e6c17a46eafabbb4573ed5cb2c615f09d4b
Davide <dadavidenapolitano@gmail.com>
2021-09-16 16:02:09 +0200

    Prima step

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 Vue.config.devtools = true;
+
+const app = new Vue({
+   el: '#app',
+   data: {
+      mail: [],
+   },
+   mounted() {
+         let index = 10;
+         for(let i = 0; i < index; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail') 
+            .then((ele) => {
+               this.mail.push(ele.data.response);
+            });
+           
+         }
+      }
+
+})
+
+
+
+
+
+
+
+// in 'Teoria' la spiegazione
+// const app = new Vue({
+//    el: '#app',
+//    data: {
+//       mail: [],
+//    },
+//    mounted() {
+//       axios // axios è una libreria
+//          .get('https://flynn.boolean.careers/exercises/api/random/mail') // tramite il .get si identifica il tipo di operazione richiesta dal client
+//          .then((ele) => {
+//              this.mailUno = ele.data.response;
+//             //  this.mailUno = ele.data.response;
+//       }); // Il metodo then() prende due parametri: il primo parametro è la funzione che verrà eseguita nel caso in cui la promise venga risolta; il secondo parametro è la funzione che verrà eseguita se la promise viene rigettata.
+     
+//    }
+
+// })
+
